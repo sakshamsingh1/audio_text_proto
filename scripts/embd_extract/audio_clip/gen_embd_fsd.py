0610a1509e75d5a455046ea0b1ceddb3f09caebd
Saksham Singh <sakshamrock121@gmail.com>
2023-05-12 13:57:20 -0400

    update gen_fsdk_audioclip

--- scripts/embd_extract/audio_clip/gen_embd_fsd.py
+++ scripts/embd_extract/audio_clip/gen_embd_fsd.py
@@ -35,38 +35,3 @@ def gen_fsdk_audioclip_embd(args):
             feat_data[file_name]['fold'] = fold
 
     torch.save(feat_data, save_path)
-
-#### CLAP model
-model, model_cfg = get_model()
-
-def save_fold_embd():
-    save_path = '/scratch/sk8974/experiments/audio_text/audio_text_dhh/data/CLAP/fsd_feat/'
-
-    feat_data = {}
-
-    train_set = FSD50k()
-    train_dataloader = DataLoader(train_set, batch_size=64, shuffle=False, collate_fn=lambda x: x, num_workers=3)
-    # import pdb; pdb.set_trace()
-
-    for batch_data in tqdm(train_dataloader):
-        # print(f'Batch {i}')
-        paths = [data[0] for data in batch_data]
-        labels = [data[1] for data in batch_data]
-        folds = [data[2] for data in batch_data]
-
-        audio_embd = get_audio_embd(paths, model, model_cfg)
-
-        for idx, embd in enumerate(audio_embd):
-            path = paths[idx]
-
-            file_name = path.split('/')[-1]
-            label = labels[idx]
-            fold = folds[idx]
-
-            feat_data[file_name] = {}
-            feat_data[file_name]['class_gt'] = label
-            feat_data[file_name]['embd'] = embd
-            feat_data[file_name]['fold'] = fold
-
-    torch.save(feat_data, save_path + 'feat_data_finetuned.pt')
-
