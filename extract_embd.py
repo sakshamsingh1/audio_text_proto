0fbedba43699a201c4c35aabcc7d26f402720204
Saksham Singh <sakshamrock121@gmail.com>
2023-05-14 17:14:05 -0400

    fix

--- extract_embd.py
+++ extract_embd.py
@@ -6,7 +6,7 @@ from scripts.embd_extract.get_embd import gen_embd
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--model_type", type=str, default="clap", help="clap or audioclip")
+    parser.add_argument("--model_type", type=str, default="audioclip", help="clap or audioclip")
     parser.add_argument("--dataset_name", type=str, default="esc-50", help="esc50, us8k, fsd50k")
     parser.add_argument("--num_workers", type=int, default=1, help="num_workers")
     args = parser.parse_args()
